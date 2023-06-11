57770a529268a34530a47edeb4f9b0aab76e23cb
pratamaekky <50559449+pratamaekky@users.noreply.github.com>
2023-06-11 22:41:55 +0700

    Update products.controller.js

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,5 +1,8 @@
-const getProducts = (req, res) => {
-    return "Hai ini untuk ambil products";
+
+const getProducts = (request, res) => {
+    const products = request.pgsql.query('SELECT name, sku, image, price, stock FROM products')
+    // status 
+    return products.rows;
 }
  
 module.exports = {
